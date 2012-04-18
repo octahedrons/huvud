321eec92a7d11863851ce331dae60f33a4778717
Patrik Ragnarsson <patrik@starkast.net>
2012-04-18 14:28:18 +0200

    The headers

--- hi.rb
+++ hi.rb
@@ -2,5 +2,23 @@ require 'sinatra'
 
 get '/hi' do
   "Hello World!"
+
+  "<p>"
+
+  "<ul>"
+  "<li>"
+  "request.env['HTTP_X_REAL_IP']: #{request.env['HTTP_X_REAL_IP']}"
+  "</li>"
+  "<li>"
+  "request.env['HTTP_X_REAL_IP']: #{request.env['REMOTE_ADDR']}"
+  "</li>"
+  "<li>"
+  "request.ip: #{request.ip}"
+  "</li>"
+  "<li>"
+  "request.remote_ip: #{request.remote_ip}"
+
+  "</ul>
+
 end
 
