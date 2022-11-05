dcc9a2f93c934f9f951a99b3cdae13d988c7c2e1
Patrik Ragnarsson <patrik@starkast.net>
2022-11-05 18:57:26 +0100

    Update `AppMetadata` for Fly.io

--- lib/app_metadata.rb
+++ lib/app_metadata.rb
@@ -1,15 +1,15 @@
-# See Heroku Labs: Dyno Metadata for more information
-# https://devcenter.heroku.com/articles/dyno-metadata
+# frozen_string_literal: true
 
+# https://github.com/dentarg/fly/commit/b8685c570ea849367f1a719121bcba0bf6645b3f
 module AppMetadata
   module_function
 
   def app_name
-    ENV.fetch("HEROKU_APP_NAME") { "huvud-dev" }
+    ENV.fetch("FLY_APP_NAME") { "huvud-dev" }
   end
 
   def commit
-    ENV.fetch("HEROKU_SLUG_COMMIT") { "bad0a554069af49b3de35b8e8c26765c1dba9ff02" }
+    ENV.fetch("RELEASE_COMMIT") { "bad0a554069af49b3de35b8e8c26765c1dba9ff02" }
   end
 
   def short_commit
